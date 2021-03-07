2bcf4ec610eadca582d08d6ea8bc7e7e8246e060
yusufgokdemir <71265842+yusufgokdemir@users.noreply.github.com>
2021-03-07 21:15:22 +0100

    Create dortislem.js

---
+++ dortislem.js
@@ -0,0 +1,5 @@
+const hesaplama = require("./function.js");
+console.log("Toplama = "+hesaplama.toplama(30,20));
+console.log("Cikarma = "+hesaplama.cikarma(30,20));
+console.log("Carpma = "+hesaplama.carpma(30,20));
+console.log("Bolme = "+hesaplama.bolme(30,20));
